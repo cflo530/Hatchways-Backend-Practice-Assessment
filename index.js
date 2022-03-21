6a32384f81bebad7a54030b36b15fb2b9763c4f7
Carlos Flores <cflo530@me.com>
2022-03-21 11:14:33 -0700

    Completed part 3 of practice assessment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const fs = require('fs');
 const data = require('./data.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 // GET Route to fetch all Recipes
 app.get('/recipes', (req, res) => {
@@ -13,7 +16,7 @@ app.get('/recipes', (req, res) => {
         obj.recipeNames.push(data.recipes[key].name);
     }
 
-    res.send(obj);
+    return res.send(obj);
 });
 
 // GET Route to fetch a single Recipe
@@ -26,17 +29,36 @@ app.get('/recipes/details/:recipe', (req, res) => {
         if (data.recipes[key].name === req.params.recipe) {
             obj.details.push(data.recipes[key].ingredients);
             obj.numSteps.push(data.recipes[key].instructions.length);
-            res.send(obj);
+            return res.send(obj);
         }
     }
 
     if(!obj) {
-        res.send({});
+         return res.send({});
     }
     
 });
 
 // POST Route to add new Recipe
+app.post('/recipes', (req, res) => {
+    const recipeName = req.body.name;
+    const recipes = data.recipes.map(index => index.name);
 
+    if (recipes.includes(recipeName)) {
+        return res.status(400).json({ error: "Recipe already exists" });
+    } else {
+        data.recipes.push(req.body);
+        let temp = JSON.stringify(data, null, 2);
+
+        fs.writeFile('data.json', temp, (err) => {
+            if(err) {
+                return res.status(500).json({ message: err });
+            }
+        });
+
+        return res.status(201).json({ message: 'New recipe added'});
+    }
+
+});
 
 app.listen(PORT, () => console.log(`Running on ${PORT}...`));
